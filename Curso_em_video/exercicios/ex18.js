6fc2dac3a5bfbed012a22c08e297e165caef560f
João Victor Soares Nascimento <61801708+joao-barabba@users.noreply.github.com>
2021-05-28 16:38:27 -0300

    Add files via upload

---
+++ Curso_em_video/exercicios/ex18.js
@@ -0,0 +1,15 @@
+/*let num = ["Paulo","Marcos",6,false]
+ for(let pos =0; pos<num.length; pos++){
+     console.log(`Seu array é ${num[pos]}`)
+ }*/
+
+ let valores = [0,3,5,1,2,4]  
+ let pos = num.indexOf(9) 
+    if(pos == -1){
+        console.log(`Este valor não está no objeto`)
+    } else{
+        console.log(`Valor encontrado: ${valores[pos]}`)
+   /* for(let pos in valores){// Sintaxe permitida para for no ES6, qunado é um objeto
+        console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    }*/
+}
